41eb668c4b99d1645728d9c26d00ea8ee4b3939b
King Koala <koala@yeetit.xyz>
2024-10-03 12:45:18 +0100

    Reproduce error

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,46 +1,13 @@
 'use client'
 
-import {useAccount, useConnect, useDisconnect} from 'wagmi'
+import {UserInfoApp} from "./user-info-app";
+import {UserInfoPackage} from "@repo/ui/user-info-package";
 
 function App() {
-  const account = useAccount()
-  const {connectors, connect, status, error} = useConnect()
-  const {disconnect} = useDisconnect()
-
   return (
     <>
-      <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br/>
-          addresses: {JSON.stringify(account.addresses)}
-          <br/>
-          chainId: {account.chainId}
-        </div>
-
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({connector})}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
+      <UserInfoApp/>
+      <UserInfoPackage/>
     </>
   )
 }
